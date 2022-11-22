29c864dbd0e9baa0385714a6987ce74405f87b0d
Nhat Minh <67325822+nhatminhnagoya@users.noreply.github.com>
2022-11-22 16:13:58 +0900

    responseJson name changed

--- src/components/loadOptions.js
+++ src/components/loadOptions.js
@@ -6,9 +6,9 @@ async function loadOptions(inputValue) {
       `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
       geoApiOptions
     );
-    const response_1 = await response.json();
+    const responseJson = await response.json();
     return {
-      options: response_1.data.map((city) => {
+      options: responseJson.data.map((city) => {
         return {
           value: `${city.latitude} ${city.longitude}`,
           label: `${city.name}, ${city.countryCode}`,
